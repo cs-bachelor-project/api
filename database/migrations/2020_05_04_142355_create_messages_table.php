453000b9fd23a8741e2411963e71c2cc68151666
unknown <ullhaq@hotmail.com>
2020-05-05 12:47:25 +0200

    table name typo

--- database/migrations/2020_05_04_142355_create_messages_table.php
+++ database/migrations/2020_05_04_142355_create_messages_table.php
@@ -32,6 +32,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_messages');
+        Schema::dropIfExists('messages');
     }
 }
